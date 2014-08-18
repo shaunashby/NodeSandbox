9e5a3bf112c175ef2a4ee06875ab3203724d82b2
Shaun Ashby <Shaun.Ashby@gmail.com>
2014-08-18 15:11:54 +0200

    Add an AMQP receive example.

---
+++ messaging/Recv.js
@@ -0,0 +1,34 @@
+//____________________________________________________________________
+// File: Recv.js
+//____________________________________________________________________
+//
+// Author: Shaun Ashby <shaun@ashby.ch>
+// Created: 2014-08-18 15:08:17+0200
+// Revision: $Id$
+// Description: An example AMQP receive client.
+//
+// Copyright (C) 2014 Shaun Ashby
+//
+//
+//--------------------------------------------------------------------
+var amqp = require('amqplib');
+
+var AMQP_URI = "amqp://10.1.38.2";
+
+amqp.connect(AMQP_URI).then(function(conn) {
+  process.once('SIGINT', function() { conn.close(); });
+  return conn.createChannel().then(function(ch) {
+    
+    var ok = ch.assertQueue('hello', {durable: false});
+    
+    ok = ok.then(function(_qok) {
+      return ch.consume('hello', function(msg) {
+        console.log(" [x] Received '%s'", msg.content.toString());
+      }, {noAck: true});
+    });
+    
+    return ok.then(function(_consumeOk) {
+      console.log(' [*] Waiting for messages. To exit press CTRL+C');
+    });
+  });
+}).then(null, console.warn);
